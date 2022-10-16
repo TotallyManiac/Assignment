2f3ca0d1887157c9cae567dab55900ddb5efd6cc
Gauri Sharma <115906445+TotallyManiac@users.noreply.github.com>
2022-10-16 21:16:46 +1100

    Update moderator.py

--- moderator.py
+++ moderator.py
@@ -1,4 +1,8 @@
-# for part 3
+"""
+Author:
+SID:
+Unikey:
+"""
 import sys 
 ls = []
 i = 1 
@@ -11,53 +15,63 @@ while True:
     if ls.count('-task')==0:
         print("No task arguments provided.")
         break
-    if ls.count("-forum")==0:
+    
+    elif ls.count("-forum")==0:
         print("No forum arguments provided.")
         break
-    if ls.count("-people")==0:
+    elif ls.count("-people")==0:
         print("No people arguments provided.")
         break
-    if ls.count("-log")==0:
+    elif ls.count("-log")==0:
         print("No log arguments provided.")
         break 
-    if ls.count("-words")==0:
+    elif ls.count("-words")==0:
         print("No words arguments provided.")
         break
-    if ls.count("-forum")>0:
+    elif ls.count("-forum")>0:
+        s = '-forum'
+        index = []
         i = 0
-        while i<len(ls):
-            if ls[i] == "-forum":
-                ele = ls[i+1]
-                try:
-                    f = open(ele,'r')
-                except:
-                    print(f"{ele} cannot be read.")
-            i+=1
-        break
+        while i < len(ls):
+            if s == ls[i]:
+                index.append(i)
+            i += 1
+        new_index = ls[index[0]+1]
+        try:
+            f = open(new_index,'r')
+        except FileNotFoundError:
+            print(f"{new_index} cannot be read.")
+            exit()
 
-    if ls.count("-words")>0:
+    elif ls.count("-words")>0:
+        s = '-words'
+        index = []
         i = 0
-        while i<len(ls):
-            if ls[i] == "-words":
-                ele = ls[i+1]
-                try:
-                    f = open(ele,'r')
-                except:
-                    print(f"{ele} cannot be read.")
-            i+=1
-        break
+        while i < len(ls):
+            if s == ls[i]:
+                index.append(i)
+            i += 1
+        new_index = ls[index[0]+1]
+        try:
+            f = open(new_index,'r')
+        except FileNotFoundError:
+            print(f"{new_index} cannot be read.")
+            exit()
      
-    if ls.count("-people")>0:
-        j = 0
-        while j<len(ls):
-            if ls[j] == "-people":
-                fn = ls[j+1]
-                try:
-                    f = open(fn,'r')
-                except:
-                    print(f"{fn} cannot be read.")
-            i+=1
-        break
+    elif ls.count("-people")>0:
+        s = '-people'
+        index = []
+        i = 0
+        while i < len(ls):
+            if s == ls[i]:
+                index.append(i)
+            i += 1
+        new_index = ls[index[0]+1]
+        try:
+            f = open(new_index,'r')
+        except FileNotFoundError:
+            print(f"{new_index} cannot be read.")
+            exit()
 
     #elif ls.count("bad.people")>0:
         #print("bad.people cannot be read.") # this is hardcoded  
@@ -65,7 +79,7 @@ while True:
     #if ls.count("bad.forum")>0:
         #print("bad.forum cannot be read.") # this is hardcoded  
         #break
-    if ls.count("rank_people")==0:
+    elif ls.count("rank_people")==0:
         print("Task argument is invalid.") # this is also hardcoded       
     else:
         print("Moderator program starting...")
