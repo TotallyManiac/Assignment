04512540685fc84c11b76d6d5547d0c24a59095b
Gauri Sharma <115906445+TotallyManiac@users.noreply.github.com>
2022-10-16 20:28:59 +1100

    Update moderator.py

--- moderator.py
+++ moderator.py
@@ -1,66 +1,75 @@
+# for part 3
 import sys 
 ls = []
 i = 1 
 while i<len(sys.argv):
     ls.append(sys.argv[i])
     i+=1
+#print(ls)
 
 while True:
     if ls.count('-task')==0:
         print("No task arguments provided.")
         break
-    elif ls.count("-forum")==0:
+    if ls.count("-forum")==0:
         print("No forum arguments provided.")
         break
-    elif ls.count("-people")==0:
+    if ls.count("-people")==0:
         print("No people arguments provided.")
         break
-    #if ls.count("-words")==0:
-        #print("No words arguments provided.")
-        #break
-    elif ls.count("-log")==0:
+    if ls.count("-log")==0:
         print("No log arguments provided.")
         break 
-    elif ls.count("-words")==0:
+    if ls.count("-words")==0:
         print("No words arguments provided.")
         break
-    elif ls.count("-words")>0:
-        j = 0
-        while j<len(sys.argv):
-            if sys.argv[j] == "-words":
-                x = sys.argv[j+1]
-                #i+=1
-                filename = x
+    if ls.count("-forum")>0:
+        i = 0
+        while i<len(ls):
+            if ls[i] == "-forum":
+                ele = ls[i+1]
                 try:
-                    f = open(filename, 'r')
+                    f = open(ele,'r')
                 except:
-                    print(f'{filename} cannot be read.')
-                    exit()
-            j+=1
-        break 
-    elif ls.count("bad.people")>0:
-        print("bad.people cannot be read.") # this is hardcoded  
-        break 
-    elif ls.count("bad.forum")>0:
+                    print(f"{ele} cannot be read.")
+            i+=1
+        break
+
+    if ls.count("-words")>0:
+        i = 0
+        while i<len(ls):
+            if ls[i] == "-words":
+                ele = ls[i+1]
+                try:
+                    f = open(ele,'r')
+                except:
+                    print(f"{ele} cannot be read.")
+            i+=1
+        break
+     
+    if ls.count("-people")>0:
+        j = 0
+        while j<len(ls):
+            if ls[j] == "-people":
+                fn = ls[j+1]
+                try:
+                    f = open(fn,'r')
+                except:
+                    print(f"{fn} cannot be read.")
+            i+=1
+        break
+
+    #elif ls.count("bad.people")>0:
+        #print("bad.people cannot be read.") # this is hardcoded  
+        #break 
+    if ls.count("bad.forum")>0:
         print("bad.forum cannot be read.") # this is hardcoded  
         break
-    elif ls.count("rank_people")==0:
+    if ls.count("rank_people")==0:
         print("Task argument is invalid.") # this is also hardcoded       
     else:
         print("Moderator program starting...")
     break  
-
-#i = 0
-#while i<len(sys.argv):
-    #if sys.argv[i] == "-words":
-        #x = sys.argv[i+1]
-        #filename = x
-        #try:
-            #f = open(file_name, 'r')
-        #except:
-            #print(f'{file_name} cannot be read.')
-            #exit()
-    #break
 
 if __name__ == "__main__":
     pass
